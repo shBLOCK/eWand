fc57855d96fa6425e8ef1b0c2ef8f2eff914d60b
shBLOCK <shblock3@gmail.com>
2024-06-30 19:53:15 +0800

    Fix IO expander

--- software/core/src/misc/io_exp.c
+++ software/core/src/misc/io_exp.c
@@ -4,8 +4,11 @@
 #include "io_exp.h"
 
 void io_exp_init() {
-    reg_output = i2c_bus_read_reg8(IO_EXP_ADDR, 0x01, false);
-    reg_inv = i2c_bus_read_reg8(IO_EXP_ADDR, 0x02, false);
-    reg_dir = i2c_bus_read_reg8(IO_EXP_ADDR, 0x03, false);
+    gpio_init(IO_EXP_PIN_INT);
+    gpio_set_dir(IO_EXP_PIN_INT, GPIO_IN);
+    gpio_set_pulls(IO_EXP_PIN_INT, true, false);
+    reg_output = i2c_bus_read_reg8(IO_EXP_ADDR, 0x01);
+    reg_inv = i2c_bus_read_reg8(IO_EXP_ADDR, 0x02);
+    reg_dir = i2c_bus_read_reg8(IO_EXP_ADDR, 0x03);
     //TODO setup IRQ
 }
--- software/core/src/misc/io_exp.h
+++ software/core/src/misc/io_exp.h
@@ -4,27 +4,27 @@
 #include "utils/utils.h"
 
 #define IO_EXP_PIN_INT 25
-#define IO_EXP_ADDR 0x20
+#define IO_EXP_ADDR 0x38 //TODO:??? this doesn't match the datasheet! should be 0x20
 
 void io_exp_init();
 
 static uint8_t reg_output, reg_inv, reg_dir;
 
-static inline bool io_exp_get(uint pin) {
-    return bit_get(i2c_bus_read_reg8(IO_EXP_ADDR, 0x00, false), pin);
+static inline bool io_exp_get(const uint pin) {
+    return bit_get(i2c_bus_read_reg8(IO_EXP_ADDR, 0x00), pin);
 }
 
-static inline void io_exp_put(uint pin, bool value) {
+static inline void io_exp_put(const uint pin, const bool value) {
     reg_output = bit_put(reg_output, pin, value);
-    i2c_bus_write_reg8(IO_EXP_ADDR, 0x01, reg_output, false);
+    i2c_bus_write_reg8(IO_EXP_ADDR, 0x01, reg_output);
 }
 
-static inline void io_exp_set_inv(uint pin, bool inv) {
+static inline void io_exp_set_inv(const uint pin, const bool inv) {
     reg_inv = bit_put(reg_inv, pin, inv);
-    i2c_bus_write_reg8(IO_EXP_ADDR, 0x02, reg_inv, false);
+    i2c_bus_write_reg8(IO_EXP_ADDR, 0x02, reg_inv);
 }
 
-static inline void io_exp_set_dir(uint pin, bool output) {
+static inline void io_exp_set_dir(const uint pin, const bool output) {
     reg_dir = bit_put(reg_dir, pin, output);
-    i2c_bus_write_reg8(IO_EXP_ADDR, 0x03, reg_dir, false);
+    i2c_bus_write_reg8(IO_EXP_ADDR, 0x03, reg_dir);
 }
