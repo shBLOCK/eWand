56131a3ff99ba44dd0259fc3e386975ed2014fce
shBLOCK <shblock3@gmail.com>
2024-06-30 20:19:48 +0800

    I2C bus 16bit operations

--- software/core/src/misc/i2c_bus.h
+++ software/core/src/misc/i2c_bus.h
@@ -20,4 +20,16 @@ static inline uint8_t i2c_bus_read_reg8(const uint8_t addr, const uint8_t reg) {
     return result;
 }
 
+static inline void i2c_bus_write_reg16(const uint8_t addr, const uint8_t reg, const uint16_t value) {
+    const uint8_t buf[] = {reg, value >> 8, value & 0xFF};
+    i2c_write_blocking(I2C_BUS_INST, addr, buf, 3, false);
+}
+
+static inline uint16_t i2c_bus_read_reg16(const uint8_t addr, const uint8_t reg) {
+    i2c_write_blocking(I2C_BUS_INST, addr, &reg, 1, true);
+    uint8_t result[2];
+    i2c_read_blocking(I2C_BUS_INST, addr, result, 2, false);
+    return result[0] << 8 | result[1];
+}
+
 void i2c_bus_debug_scan();
